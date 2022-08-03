13e073ea63fb12a429da97789d807b84ac0e154a
Rakhim <ahdan042@gmail.com>
2022-08-04 00:09:13 +0300

    14

--- aviato/urls.py
+++ aviato/urls.py
@@ -41,17 +41,31 @@ def data(request, id):
     id_or_phone = id
     try:
         p = Applications.objects.get(pk=id_or_phone)
-        logger.critical(p)
         if p:
-            return JsonResponse({
-                "products": p.product,
-                "address": p.address,
-                "time_update_location": p.time_update_location,
-                "price": p.price,
-                "location": p.location,
-                "phone": p.phone,
-                "id": p.pk,
-            }, safe=False)
+            if p.status == "В дороге":
+                return JsonResponse({
+                    "products": p.product,
+                    "address": p.address,
+                    "status": "Ваш товар в дороге!",
+                    "time_update_location": p.time_update_location,
+                    "price": p.price,
+                    "location": p.location,
+                    "phone": p.phone,
+                    "id": p.pk,
+                }, safe=False)
+            else:
+                return JsonResponse({
+                    "products": p.product,
+                    "address": p.address,
+                    "status": "Ваш товар подготавливается к отправке!",
+                    "time_update_location": p.time_update_location,
+                    "price": p.price,
+                    "location": p.location,
+                    "phone": p.phone,
+                    "id": p.pk,
+                }, safe=False)                
+
+
     except Exception as ex:
         logger.error(ex)
 
