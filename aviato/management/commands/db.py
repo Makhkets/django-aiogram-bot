c6a7b927807a40550d5f1a004827c810b2b70c2f
rakhim <ahdan042@gmail.com>
2022-10-24 18:20:38 +0300

    fixed price converter

--- aviato/management/commands/db.py
+++ aviato/management/commands/db.py
@@ -21,22 +21,17 @@ def convert_phone_number_to_seven(phone):
 def convert_price(price):
     convert = ""
     def string_numbers_convert(string):
-        flag = [False, False]
+        flag = False
         for i in string:
             if i.isdigit():
-                flag[0] = True
-
-        if flag[0]:
-            for i in string:
-                if i == "т":
-                    flag[1] = True
-        
-        if flag[0] and flag[1]: return True
-        return False
+                flag = True
+        return flag
         
     for string in price.split(" "):
         if string_numbers_convert(string=string):
-            convert += string.replace("т", "000")
+            for i in string:
+                if i == "т" or i.isdigit():
+                    convert += i.replace("т", "000")
 
 
     return int(str(convert) \
--- scrapping_xlsx.py
+++ scrapping_xlsx.py
@@ -1,25 +1,19 @@
-import re
 
 
 def convert_price(price):
     convert = ""
     def string_numbers_convert(string):
-        flag = [False, False]
+        flag = False
         for i in string:
             if i.isdigit():
-                flag[0] = True
-
-        if flag[0]:
-            for i in string:
-                if i == "т":
-                    flag[1] = True
-        
-        if flag[0] and flag[1]: return True
-        return False
+                flag = True
+        return flag
         
     for string in price.split(" "):
         if string_numbers_convert(string=string):
-            convert += string.replace("т", "000")
+            for i in string:
+                if i == "т" or i.isdigit():
+                    convert += i.replace("т", "000")
 
 
     return int(str(convert) \
@@ -28,4 +22,4 @@ def convert_price(price):
                 .replace(";", "")
     )
 
-print(convert_price("рублей 1т,"))
+print(convert_price("Итого с доставкой: 56000₽"))
